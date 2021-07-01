8ba455449af8afdc9c7ed85bb949de421abdb0fe
Johannes Borgqvist <johannes.borgqvist@gmail.com>
2021-07-01 15:38:16 +0100

    Add generation of spherical mesh

--- Code/generate_spherical_mesh.py
+++ Code/generate_spherical_mesh.py
@@ -1,118 +1,239 @@
-# ------------------------------------------------------------------------------
-#
-#  Gmsh Python tutorial 4
-#
-#  Holes in surfaces, annotations, entity colors
-#
-# ------------------------------------------------------------------------------
-
-import gmsh
-import math
-import sys
-
+# =================================================================================
+# =================================================================================
+# Script:"generate_spherical_mesh"
+# Date: 2021-06-28
+# Implemented by: Johannes Borgqvist
+# Description:
+# The program generates a spherical mesh with
+# a given radius, and it generates a given
+# number of holes in the mesh determined by
+# the input.
+# =================================================================================
+# =================================================================================
+# Import Libraries
+# =================================================================================
+# =================================================================================
+import gmsh  # For generating the meshes
+import math  # For using mathematical functions
+import sys  # Needed by gmsh to launch the GUI, i.e. open the window with the plot
+import numpy as np  # Needed to create arrays
+# =================================================================================
+# =================================================================================
+# Code for generating the mesh
+# =================================================================================
+# =================================================================================
+# Initialise the Gmsh framework
 gmsh.initialize()
-
-gmsh.model.add("t4")
-
-cm = 1e-02
-e1 = 4.5 * cm
-e2 = 6 * cm / 2
-e3 = 5 * cm / 2
-h1 = 5 * cm
-h2 = 10 * cm
-h3 = 5 * cm
-h4 = 2 * cm
-h5 = 4.5 * cm
-R1 = 1 * cm
-R2 = 1.5 * cm
-r = 1 * cm
-Lc1 = 0.01
-Lc2 = 0.003
-
-
-def hypot(a, b):
-    return math.sqrt(a * a + b * b)
-
-
-ccos = (-h5 * R1 + e2 * hypot(h5, hypot(e2, R1))) / (h5 * h5 + e2 * e2)
-ssin = math.sqrt(1 - ccos * ccos)
-
+# Add a model entity
+gmsh.model.add("sphere_with_holes")
 # We start by defining some points and some lines. To make the code shorter we
 # can redefine a namespace:
-factory = gmsh.model.geo
-factory.addPoint(-e1 - e2, 0, 0, Lc1, 1)
-factory.addPoint(-e1 - e2, h1, 0, Lc1, 2)
-factory.addPoint(-e3 - r, h1, 0, Lc2, 3)
-factory.addPoint(-e3 - r, h1 + r, 0, Lc2, 4)
-factory.addPoint(-e3, h1 + r, 0, Lc2, 5)
-factory.addPoint(-e3, h1 + h2, 0, Lc1, 6)
-factory.addPoint(e3, h1 + h2, 0, Lc1, 7)
-factory.addPoint(e3, h1 + r, 0, Lc2, 8)
-factory.addPoint(e3 + r, h1 + r, 0, Lc2, 9)
-factory.addPoint(e3 + r, h1, 0, Lc2, 10)
-factory.addPoint(e1 + e2, h1, 0, Lc1, 11)
-factory.addPoint(e1 + e2, 0, 0, Lc1, 12)
-factory.addPoint(e2, 0, 0, Lc1, 13)
-
-factory.addPoint(R1 / ssin, h5 + R1 * ccos, 0, Lc2, 14)
-factory.addPoint(0, h5, 0, Lc2, 15)
-factory.addPoint(-R1 / ssin, h5 + R1 * ccos, 0, Lc2, 16)
-factory.addPoint(-e2, 0.0, 0, Lc1, 17)
-
-factory.addPoint(-R2, h1 + h3, 0, Lc2, 18)
-factory.addPoint(-R2, h1 + h3 + h4, 0, Lc2, 19)
-factory.addPoint(0, h1 + h3 + h4, 0, Lc2, 20)
-factory.addPoint(R2, h1 + h3 + h4, 0, Lc2, 21)
-factory.addPoint(R2, h1 + h3, 0, Lc2, 22)
-factory.addPoint(0, h1 + h3, 0, Lc2, 23)
-
-factory.addPoint(0, h1 + h3 + h4 + R2, 0, Lc2, 24)
-factory.addPoint(0, h1 + h3 - R2, 0, Lc2, 25)
-
-factory.addLine(1, 17, 1)
-factory.addLine(17, 16, 2)
-
-# Gmsh provides other curve primitives than straight lines: splines, B-splines,
-# circle arcs, ellipse arcs, etc. Here we define a new circle arc, starting at
-# point 14 and ending at point 16, with the circle's center being the point 15:
-factory.addCircleArc(14, 15, 16, 3)
-
-# Note that, in Gmsh, circle arcs should always be smaller than Pi. The
-# OpenCASCADE geometry kernel does not have this limitation.
-
-# We can then define additional lines and circles, as well as a new surface:
-factory.addLine(14, 13, 4)
-factory.addLine(13, 12, 5)
-factory.addLine(12, 11, 6)
-factory.addLine(11, 10, 7)
-factory.addCircleArc(8, 9, 10, 8)
-factory.addLine(8, 7, 9)
-factory.addLine(7, 6, 10)
-factory.addLine(6, 5, 11)
-factory.addCircleArc(3, 4, 5, 12)
-factory.addLine(3, 2, 13)
-factory.addLine(2, 1, 14)
-factory.addLine(18, 19, 15)
-factory.addCircleArc(21, 20, 24, 16)
-factory.addCircleArc(24, 20, 19, 17)
-factory.addCircleArc(18, 23, 25, 18)
-factory.addCircleArc(25, 23, 22, 19)
-factory.addLine(21, 22, 20)
-
-factory.addCurveLoop([17, -15, 18, 19, -20, 16], 21)
-factory.addPlaneSurface([21], 22)
-
-# But we still need to define the exterior surface. Since this surface has a
-# hole, its definition now requires two curves loops:
-factory.addCurveLoop([11, -12, 13, 14, 1, 2, -3, 4, 5, 6, 7, -8, 9, 10], 23)
-factory.addPlaneSurface([23, 21], 24)
-
-# As a general rule, if a surface has N holes, it is defined by N+1 curve loops:
-# the first loop defines the exterior boundary; the other loops define the
-# boundaries of the holes.
-
-factory.synchronize()
-
+sphere_with_holes = gmsh.model.geo
+# -------------------------------------------------------------------
+# Allocating memory for all lists and vectors
+# -------------------------------------------------------------------
+# Define a centimetre
+cm = 1e-02
+Lc1 = 0.01
+r = 1 * cm
+# Define the theta angle
+#theta_vec = np.linspace(0,2*math.pi,100)
+theta_vec = np.linspace(math.pi/6,math.pi/3,2)
+#theta_vec = np.linspace(0,math.pi/10,4)
+# Define the phi angle
+#phi_vec = np.linspace(0,math.pi,100)
+phi_vec = np.linspace(math.pi/10,math.pi,2)
+# Define a corresponding mesh
+phi_list = phi_vec.tolist()
+# Define all points that we want to add
+point_list = []
+# Define a point counter
+point_counter = 1
+# Define a line counter
+line_counter = 1
+# Define a curve counter
+curve_counter = 1
+# Define a list for the lines
+line_list = []
+# Define a list for the curves
+curve_list = []
+# -------------------------------------------------------------------
+# Looping over angles and defining all points, lines and curve loops 
+# -------------------------------------------------------------------
+# Make a list of previous points
+prev_points = []
+# Make a list of previous lines
+prev_lines = []
+# Loop over the theta vector (x,y-axes)
+for theta_index in range(len(theta_vec)-1):
+    print("\tTheta iter %d out of %d"%(theta_index+1,len(theta_vec)-1))
+    # Extract a pair of theta angles
+    theta_pair = [theta_vec[theta_index], theta_vec[theta_index +1]]
+    print("\tTheta angles:\t%s"%(str(theta_pair)))
+    # Previous index
+    prev_index = 0
+    # Loop over the phi angles (z-axis)
+    for phi_index in range(len(phi_vec)-1):    
+        print("\t\tPhi iter %d out of %d"%(phi_index+1,len(phi_vec)-1))
+        # Extract a pair of phi angles
+        phi_pair = [phi_vec[phi_index], phi_vec[phi_index + 1 ]]
+        # Now, we will extract four points which will
+        # be used to define four lines and then a curve loop
+        # using these lines
+        #---------------------------------------------------------
+        # POINT 1
+        if theta_index == 0:
+            # Define the 3D point by its cartesian coordinates
+            x1 = r * np.cos(theta_pair[0]) * np.sin(phi_pair[0])
+            y1 = r * np.sin(theta_pair[0]) * np.sin(phi_pair[0])
+            z1 = r * np.cos(phi_pair[0])
+            # Add the point to our list
+            point_list.append((x1,y1,z1,point_counter))
+            # Add the point to our previous list
+            prev_points.append((x1,y1,z1,point_counter))            
+            # Set the point label
+            point_label = point_counter
+            # Increase the tag number of the point
+            point_counter += 1
+        else:
+            x1 = prev_points[prev_index][0]
+            y1 = prev_points[prev_index][1]
+            z1 = prev_points[prev_index][2]
+            point_label = prev_points[prev_index][3]
+            prev_index += 1
+        # Add the point to the mesh
+        sphere_with_holes.addPoint(x1, y1, z1, Lc1, point_label)
+        #---------------------------------------------------------
+        # POINT 2
+        # Define the 3D point by its cartesian coordinates
+        x2 = r * np.cos(theta_pair[1]) * np.sin(phi_pair[0])
+        y2 = r * np.sin(theta_pair[1]) * np.sin(phi_pair[0])
+        z2 = r * np.cos(phi_pair[0])
+        # Add the point to the mesh
+        sphere_with_holes.addPoint(x2, y2, z2, Lc1, point_counter)
+        # Add the point to our list
+        point_list.append((x2,y2,z2,point_counter))
+        # Increase the tag number of the point
+        point_counter += 1
+        #---------------------------------------------------------
+        # POINT 3
+        # Define the 3D point by its cartesian coordinates
+        x3 = r * np.cos(theta_pair[1]) * np.sin(phi_pair[1])
+        y3 = r * np.sin(theta_pair[1]) * np.sin(phi_pair[1])
+        z3 = r * np.cos(phi_pair[1])
+        # Add the point to the mesh
+        sphere_with_holes.addPoint(x3, y3, z3, Lc1, point_counter)
+        # Add the point to our list
+        point_list.append((x3,y3,z3,point_counter))
+        # Increase the tag number of the point
+        point_counter += 1
+        #---------------------------------------------------------
+        # POINT 4
+        if theta_index == 0:
+            # Define the 3D point by its cartesian coordinates
+            x4 = r * np.cos(theta_pair[0]) * np.sin(phi_pair[1])
+            y4 = r * np.sin(theta_pair[0]) * np.sin(phi_pair[1])
+            z4 = r * np.cos(phi_pair[1])
+            # Add the point to our list
+            point_list.append((x4,y4,z4,point_counter))
+            # Add the point to our previous list
+            prev_points.append((x4,y4,z4,point_counter))            
+            # Set the point label
+            point_label = point_counter
+            # Increase the tag number of the point
+            point_counter += 1
+        else:
+            x4 = prev_points[prev_index][0]
+            y4 = prev_points[prev_index][1]
+            z4 = prev_points[prev_index][2]
+            point_label = prev_points[prev_index][3]
+            prev_index += 1            
+        # Add the point to the mesh
+        sphere_with_holes.addPoint(x4, y4, z4, Lc1, point_label)
+        #---------------------------------------------------------
+        # POINT 1 (AGAIN)
+        if theta_index == 0:
+            # Define the 3D point by its cartesian coordinates
+            x1 = r * np.cos(theta_pair[0]) * np.sin(phi_pair[0])
+            y1 = r * np.sin(theta_pair[0]) * np.sin(phi_pair[0])
+            z1 = r * np.cos(phi_pair[0])
+            # Add the point to our list
+            point_list.append((x1,y1,z1,point_counter))
+            # Add the point to our previous list
+            prev_points.append((x1,y1,z1,point_counter))            
+            # Set the point label
+            point_label = point_counter-4
+            # Increase the tag number of the point
+            point_counter += 1
+        else:
+            x1 = prev_points[prev_index][0]
+            y1 = prev_points[prev_index][1]
+            z1 = prev_points[prev_index][2]
+            point_label = prev_points[prev_index][3]
+            prev_index += 1
+            # Add the point to our list
+            point_list.append((x1,y1,z1,point_label))
+            # Add the point to our previous list
+            prev_points.append((x1,y1,z1,point_label))                        
+        # Add the point to the mesh
+        #sphere_with_holes.addPoint(x1, y1, z1, Lc1, point_label)
+        #---------------------------------------------------------
+        #-----------------------------------------------------------
+        # THE LINES
+        #-----------------------------------------------------------
+        # The line counter
+        line_counter = point_counter
+        # Loop over the points and draw lines between them
+        for i in range(0,len(point_list)):
+            # Make sure that we loop until the second last point
+            if i < len(point_list)-1:
+                # Add the point
+                sphere_with_holes.addLine(point_list[i][3], point_list[i+1][3], line_counter)
+                # Append the new line to our list of lines
+                line_list.append(line_counter)
+                # Update the counter for the lines
+                line_counter += 1
+        #-----------------------------------------------------------
+        # THE CURVE LOOPS
+        #-----------------------------------------------------------
+        # Define a counter controlling the tag of the curve loop
+        curve_counter = line_counter+1
+        # Add the curve loop  to the mesh
+        sphere_with_holes.addCurveLoop(line_list, curve_counter)
+        # Append our newly defined curve to our curve list
+        curve_list.append(curve_counter)
+        #-----------------------------------------------------------
+        # RESET THE LISTS FOR THE POINTS AND THE LINES
+        #-----------------------------------------------------------
+        point_list = []
+        line_list = []
+        # Lastly, update the point_counter
+        point_counter = curve_counter + 1 
+        # End of inner for-loop that has to do with phi
+# -------------------------------------------------------------------
+# Assembling the surfaces over the sphere
+# -------------------------------------------------------------------
+# Introduce a counter for the surface
+surface_counter = point_counter
+# List of surfaces
+surface_list = [surface_counter]
+# Loop over the various curves and add them to the mesh
+for curve in curve_list:
+    # Add the curve loop as a surface
+    sphere_with_holes.addPlaneSurface([curve], surface_counter)
+    # Increase the counter keeping track of the surfaces
+    surface_counter += 1
+    # Save the surface at hand
+    surface_list.append(surface_counter)
+# -------------------------------------------------------------------
+# Generating the mesh
+# -------------------------------------------------------------------
+# Synchronise everything with Gmsh
+sphere_with_holes.synchronize()
+# Set the colours of the surfaces
+gmsh.model.setColor([(2, surface) for surface in surface_list], 221, 28, 119)  # Magenta
+# For nice colours, please see (https://colorbrewer2.org/)
+#----------------------------------
 # Finally, we can add some comments by creating a post-processing view
 # containing some strings:
 v = gmsh.view.add("comments")
@@ -162,19 +283,11 @@ gmsh.option.setString(
     "Geometry.DoubleClickedLineCommand",
     "Printf('Curve %g has been double-clicked!', "
     "Geometry.DoubleClickedEntityTag);")
-
-# We can also change the color of some entities:
-gmsh.model.setColor([(2, 22)], 127, 127, 127)  # Gray50
-gmsh.model.setColor([(2, 24)], 160, 32, 240)  # Purple
-gmsh.model.setColor([(1, i) for i in range(1, 15)], 255, 0, 0)  # Red
-gmsh.model.setColor([(1, i) for i in range(15, 21)], 255, 255, 0)  # Yellow
-
+# Generate the two dimensional mesh (as we work with surfaces) 
 gmsh.model.mesh.generate(2)
-
+# Write the mesh to our file
 gmsh.write("../Meshes/sphere.msh")
-
 # Launch the GUI to see the results:
 if '-nopopup' not in sys.argv:
     gmsh.fltk.run()
-
 gmsh.finalize()
---
+++ TASK_LIST.md
@@ -0,0 +1,29 @@
+# Sammanfattning av planen
+- [x] Sätt upp en github,
+- [ ] Lägg till en README fil,
+- [x] Sätt upp grenar eller "branches" på githuben,
+- [x] Skapa en klassisk mappstruktur på Github,
+- [ ] Generera ett sfärisk nät via python mha gmsh,
+- [ ] Generera ett sfärisk nät med ett sfärisk via python mha gmsh,
+- [ ] Implementera så att meshen kan ses via paraview och python,
+- [ ] Skriv ett enkelt skript som läser in meshen via Fenics,
+- [ ] Sätt upp en variationsformulering,
+- [ ] Implementera ett adaptivt tidssteg, 
+- [ ] Reproducera fig. 4.3-4.6 i Chaplain (2001),
+- [ ] Lägg till ett eller flera budscars. Beräkna perturberade egenvärden och undersök om de lämnar det instabila intervallet,
+- [ ] Bekräfta de teoretiska resultaten genom att lösa systemet numeriskt,
+- [ ] Inför en växande sfär med radie R(t),
+- [ ] Undersök mönsterbildning på den växande sfären,
+- [ ] Undersök hur närvaron av budscars påverkar möjligheten för mönsterbildning på den växande sfären,
+- [x] Såhär kryssar man i en ruta. 
+
+
+
+## Branches:
+Här är en väldigt bra [länk](https://thenewstack.io/dont-mess-with-the-master-working-with-branches-in-git-and-github/) för hur man jobbar med grenar eller "branches" på github.
+
+Jag har gjort en gren för varje deltagare i projektet:
+1. Johannes's branch,
+2. Philip's branch.
+  
+
