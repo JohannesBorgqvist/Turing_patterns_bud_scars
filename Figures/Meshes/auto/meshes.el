f3b522a16803db0c08ffe82ea5a3ad10127808ef
Johannes Borgqvist <johannes.borgqvist@gmail.com>
2021-07-06 15:14:37 +0100

    Add Figures of meshes

---
+++ Figures/Meshes/auto/meshes.el
@@ -0,0 +1,13 @@
+(TeX-add-style-hook
+ "meshes"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "bud_scar_2"
+    "standalone"
+    "standalone10"
+    "graphicx"
+    "tikz"
+    "xcolor"))
+ :latex)
+
