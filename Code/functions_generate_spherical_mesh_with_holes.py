a4fcfdc66111c80f47e25b80b49915bd30fa1df0
Johannes Borgqvist <johannes.borgqvist@gmail.com>
2021-09-01 15:43:39 +0100

    Add fine-tuned meshes

--- Code/functions_generate_spherical_mesh_with_holes.py
+++ Code/functions_generate_spherical_mesh_with_holes.py
@@ -652,7 +652,17 @@ def add_parts_of_sphere(R,phi_tuple,theta_tuple,object_counter,north_pole,south_
 def add_sphere_without_hole(R,phi_tuple,theta_tuple,object_counter,north_pole,south_pole,main_centre):
     # Define a step size in the theta direction
     #theta_step = math.pi/24
-    theta_step = math.pi/100
+    #theta_step = math.pi/75
+    #theta_step = math.pi/100
+    #theta_step = math.pi/125
+    #theta_step = math.pi/150
+    #theta_step = math.pi/175
+    #theta_step = math.pi/200
+    #theta_step = math.pi/300
+    #theta_step = math.pi/400
+    #theta_step = math.pi/500
+    theta_step = math.pi/350
+    #theta_step = math.pi/360
     # Define the number of steps based on this step size
     num_theta_steps = int(round(((theta_tuple[1]-theta_tuple[0])/(theta_step))))
     # Define our theta vector
