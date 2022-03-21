1ee4ef8d8e6665d0862651ea40bab89ca041a115
Johannes Borgqvist <borgqvist@maths.ox.ac.uk>
2022-03-21 13:17:15 +0000

    Change launch script so that we do not work with any subdomains, activation parameters or cell growth. Thus, clean up the script!

--- Code/launch_simulations_Schnakenberg_sphere_with_holes.py
+++ Code/launch_simulations_Schnakenberg_sphere_with_holes.py
@@ -38,45 +38,29 @@ steady_states = [u_0,v_0]
 print("\n\t\tThe steady states:\t\t\t(u_0,v_0)\t=\t(%0.4f,%0.4f)"%(u_0,v_0))
 print("\t\tThe critical parameters:\t\t(d_c,gamma_c)\t=\t(%0.4f,%0.4f)"%(d_c,gamma_c))
 # Set the value of the relative diffusion
-#d=5
-#d=10
-#d = 100000000000000
 d = 18
 # Set the value of the reaction strength to its critical value
 gamma = gamma_c
 # Compute minimal critical hole radius for pattern disturbance
-#n_largest = 731 # Largest eigenvalue parameter to test for.
-#n_largest = 6
 n_largest = 6
-eps_min, n_min, m_min = Schnakenberg_properties.compute_minimal_holeradius_for_pattern_disturbance(a,b,d,gamma,n,n_largest)
-hole_cylinder_radius = np.sin(eps_min)
+eps_tuple, n_tuple, m_tuple = Schnakenberg_properties.compute_minimal_holeradius_for_pattern_disturbance(a,b,d,gamma,n,n_largest)
+hole_cylinder_radius = np.sin(eps_tuple[1])
 # Print the results
 print("\n\t\tTheoretical parameter values for pattern disturbances") 
-print("\t\tThe critical radii:\t\t(geodesic, cylindric)\t=\t(%0.4f,%0.4f)"%(eps_min, hole_cylinder_radius))
-print("\t\tThe critical spectral parameters:\t\t(n,m)\t=\t(%0.4f,%0.4f)"%(n_min, m_min))
+print("\t\tThe critical radii:\t\t(geodesic, cylindric)\t=\t(%0.4f,%0.4f)"%(eps_tuple[1], hole_cylinder_radius))
+print("\t\tThe critical spectral parameters:\t\t(n,m)\t=\t(%0.4f,%0.4f)"%(n_tuple[1], m_tuple[1]))
 # Collect all parameters in a list
 parameters = [a, b, d, gamma]
 # For this experiment we have no hole in the mesh
 num_holes = 0
-#num_holes = 1
-#num_holes = 2
-#num_holes = 5
+# We have no holes, so no radius necessary
+radii_holes = []
 # Define the perturbation in the initial conditions
-#sigma = 0.01
 sigma = 1e-4
 # Define the end time for the simulations
 T = 50
-#T = 0.05
-#T = 1.5
-#T = 0.0
-#T = 2.0
 # Collect these latter two parameters in a list as well
 numerical_parameters = [sigma, T]
-# Since, we have no hole we have no adjacent region and therefore it is not meaningful to talk about local activation in this region. So we set the activation parameters to 1 meaning that we have no extra activation in the region adjacent to the hole
-activation_parameters = [1.0, 1.0]
-# We do not have any cell growth here so we set this to false
-cell_growth = False
 # Solve the FEM system with the given parameters
-FEM_toolbox.FEMFD_simulation_Schnakenberg_sphere_with_holes(num_holes,parameters,steady_states,numerical_parameters,activation_parameters,cell_growth)
-#FEM_toolbox.FEM_FD_simulation_pure_diffusion_sphere_with_holes()
+FEM_toolbox.FEMFD_simulation_Schnakenberg_sphere_with_holes(num_holes,parameters,steady_states,numerical_parameters,radii_holes)
 
