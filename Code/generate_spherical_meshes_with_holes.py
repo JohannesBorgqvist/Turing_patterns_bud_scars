8227f9edadcfc501cdb497e9eec1e952a6be3494
Johannes Borgqvist <borgqvist@maths.ox.ac.uk>
2022-03-30 10:37:36 +0100

    Add meshes with hole radii: [0. , 0.5, 1. , 1.5, 2. , 2.5, 3. , 3.5, 4. , 4.5, 5. , 5.5, 6]

--- Code/generate_spherical_meshes_with_holes.py
+++ Code/generate_spherical_meshes_with_holes.py
@@ -13,16 +13,21 @@
 # =================================================================================
 # =================================================================================
 import toolbox_generate_meshes # Home-made
+import numpy as np # For numpy functionalities
 # =================================================================================
 # =================================================================================
 # Generate the meshes
 # =================================================================================
 # =================================================================================
+# Create the hole radii in an automated fashion
+hole_radii_list = [[r] for r in np.arange(0,6.5,0.5)]
+# Allocate the lists of points defining the points on the sphere
+c0_lists = []
+c1_lists = []
 # Generate a list of points defining the points on the sphere
-c0_lists = [[(0,0,0)], [(0,0,0)], [(0,0,0)], [(0,0,0)], [(0,0,0)], [(0,0,0)]]
-c1_lists = [[(-1,1,0)], [(-1,1,0)], [(-1,1,0)], [(-1,1,0)], [(-1,1,0)], [(-1,1,0)]]
-# Generate a list of hole radii
-hole_radii_list = [[0.0],[0.05],[0.10],[0.15],[0.20], [0.25]] # Hole radii 2022-03-22
+for index in range(hole_radii_list):
+    c0_lists.append([(0,0,0)])
+    c1_lists.append([(-1,1,0)])
 # Loop over all points and radii to generate the corresponding meshes
 for list_index in range(len(hole_radii_list)):
     toolbox_generate_meshes.generate_spherical_mesh_with_holes(c0_lists[list_index],c1_lists[list_index],hole_radii_list[list_index])
