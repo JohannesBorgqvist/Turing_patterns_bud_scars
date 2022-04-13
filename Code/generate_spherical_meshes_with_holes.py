dbbd1d87b75e1091129d2aa118d45341dfa4fedf
Carl Lundholm <carl@Carls-MacBook-Pro.local>
2022-04-13 23:22:29 +0200

    Put the hole at (0,0,-1) as in Bandle et al.

--- Code/generate_spherical_meshes_with_holes.py
+++ Code/generate_spherical_meshes_with_holes.py
@@ -24,10 +24,11 @@ hole_radii_list = [[r] for r in np.arange(0,0.75,0.05)]
 # Allocate the lists of points defining the points on the sphere
 c0_lists = []
 c1_lists = []
-# Generate a list of points defining the points on the sphere
+# Generate a list of points defining the center endpoints of the hole-making cylinder
+# The spherical hole should be centered at (0,0,-1) as in Bandle
 for index in range(len(hole_radii_list)):
     c0_lists.append([(0,0,0)])
-    c1_lists.append([(-1,1,0)])
+    c1_lists.append([(0,0,-1)])
 # Loop over all points and radii to generate the corresponding meshes
 for list_index in range(len(hole_radii_list)):
     toolbox_generate_meshes.generate_spherical_mesh_with_holes(c0_lists[list_index],c1_lists[list_index],hole_radii_list[list_index])
