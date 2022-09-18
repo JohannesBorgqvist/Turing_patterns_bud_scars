cdc3bb394a42113292347cb1bce81041410b5272
Johannes Borgqvist <johannes.borgqvist@gmail.com>
2022-09-18 12:22:12 +0100

    Remove the bash script we wrote to remove files

--- Output/remove_unnecessary_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Bash version ${BASH_VERSION}..."
-for i in {0..19..1}
-do
-    echo "Entering folder $i:"
-    mv iteration_$i/u000101.vtu ./ && rm iteration_$i/* && mv u000101.vtu iteration_$i/ && ls iteration_$i    
-done
